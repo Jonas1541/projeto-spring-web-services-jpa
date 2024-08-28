cd45954394a50423457090fa0ee1b7df2e49f21b
Jonas Durau <jonasdurau@gmail.com>
2024-08-28 12:00:20 -0300

    fix OrderItemRepository ID type

--- src/main/java/com/example/demo/repositories/OrderItemRepository.java
+++ src/main/java/com/example/demo/repositories/OrderItemRepository.java
@@ -3,7 +3,8 @@ package com.example.demo.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.demo.models.OrderItem;
+import com.example.demo.models.pk.OrderItemPK;
 
-public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+public interface OrderItemRepository extends JpaRepository<OrderItem, OrderItemPK> {
     
 }
